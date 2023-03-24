0e225e7fa09d71de0810611d83837e1841e132d1
kobaken <kentafly88@gmail.com>
2023-03-24 18:20:21 +0900

    minil new

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+GraphQL::Tiny - It's new $module
+
+# SYNOPSIS
+
+    use GraphQL::Tiny;
+
+# DESCRIPTION
+
+GraphQL::Tiny is ...
+
+# LICENSE
+
+Copyright (C) kobaken.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+kobaken <kentafly88@gmail.com>
---
+++ lib/GraphQL/Tiny.pm
@@ -0,0 +1,39 @@
+package GraphQL::Tiny;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+GraphQL::Tiny - It's new $module
+
+=head1 SYNOPSIS
+
+    use GraphQL::Tiny;
+
+=head1 DESCRIPTION
+
+GraphQL::Tiny is ...
+
+=head1 LICENSE
+
+Copyright (C) kobaken.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+kobaken E<lt>kentafly88@gmail.comE<gt>
+
+=cut
+
