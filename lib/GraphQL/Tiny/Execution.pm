44e5f1f11aceafec17f6255662941830a521689e
kobaken <kentafly88@gmail.com>
2023-03-25 12:21:25 +0900

    port: execution/README.md

---
+++ lib/GraphQL/Tiny/Execution.pm
@@ -0,0 +1,23 @@
+package GraphQL::Tiny::Execution;
+use strict;
+use warnings;
+
+
+1;
+__END__
+
+
+=encoding utf-8
+
+=head1 NAME
+
+GraphQL::Tiny::Execution
+
+=head1 SYNOPSIS
+
+    use GraphQL::Tiny::Execution qw(execute);
+
+=head1 DESCRIPTION
+
+The C<GraphQL::Tiny::Execution> module is responsible for the execution phase of
+fulfilling a GraphQL request.
