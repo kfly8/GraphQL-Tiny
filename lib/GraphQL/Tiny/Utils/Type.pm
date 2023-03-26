c45d0b08d2b6ad4c9f95975f5196febcc7a83b04
kobaken <kentafly88@gmail.com>
2023-03-26 17:24:53 +0900

    add ReadonlyArray type

--- lib/GraphQL/Tiny/Utils/Type.pm
+++ lib/GraphQL/Tiny/Utils/Type.pm
@@ -12,9 +12,13 @@ our @EXPORT = qw(
     Enum
     HashRef
     Int
+    Maybe
     Null
+    Optional
+    ReadonlyArray
     Slurpy
     Str
+    Undef
     Unknown
 );
 
@@ -22,10 +26,13 @@ use Type::Utils qw(type as);
 
 use Types::Standard qw(
     Any
+    ArrayRef
     Dict
     Enum
     HashRef
     Int
+    Maybe
+    Optional
     Slurpy
     Str
     Undef
@@ -33,5 +40,21 @@ use Types::Standard qw(
 
 use constant Null => type 'Null', as Undef;
 use constant Unknown => type 'Unknown', as Any;
+
+my $ReadonlyArray = type 'ReadonlyArray', as ArrayRef,
+    name_generator => sub {
+        my ($type_name, $param) = @_;
+        sprintf '%s[%s]', $type_name, $param;
+    },
+    constraint_generator => sub {
+        my ($param) = @_;
+        my $Type = ArrayRef->of($param);
+        return sub { $Type->check(@_) }
+    };
+
+sub ReadonlyArray(;$) {
+    my ($type) = @{ $_[0] };
+    $ReadonlyArray->of($type);
+}
 
 1;
