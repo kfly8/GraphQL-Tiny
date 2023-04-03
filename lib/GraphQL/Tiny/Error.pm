812eb86f34c9b675dbe1ee4e8c0b53d4b79f0858
kobaken <kentafly88@gmail.com>
2023-04-03 17:02:00 +0900

    port: error/README.md

--- lib/GraphQL/Tiny/Error.pm
+++ lib/GraphQL/Tiny/Error.pm
@@ -29,3 +29,28 @@ use GraphQL::Tiny::Error::SyntaxError qw(syntax_error);
 use GraphQL::Tiny::Error::LocatedError qw(located_error);
 
 1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+GraphQL::Tiny::Error - GraphQL error utilities
+
+=head1 SYNOPSIS
+
+    use GraphQL::Tiny::Error qw(
+        build_graphql_error
+        GraphQLError
+        GraphQLErrorOptions
+        GraphQLFormattedError
+        GraphQLErrorExtensions
+        syntax_error
+        located_error
+    );
+
+=head1 DESCRIPTION
+
+The `graphql/error` module is responsible for creating and formatting
+GraphQL errors.
+
